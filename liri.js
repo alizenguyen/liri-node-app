e324b3be190a2736c21fb725562273f892090e1a
Alize Nguyen <alizenguyen@gmail.com>
2017-11-19 15:33:55 -0600

    twitter edits

--- liri.js
+++ liri.js
@@ -1,6 +1,11 @@
+// KEYS & VARIABLES
+
 var keys = require("./keys.js");
 
-var userInput = processargv[2];
+var userInput = process.argv[2];
+var userChoice = process.argv[3];
+
+// COMMANDS & OUTPUTS
 
 switch (userInput) {
     case "my-tweets":
@@ -8,6 +13,7 @@ switch (userInput) {
         break;
 
     case "spotify-this-song":
+        spotifySong();
         break;
 
     case "movie-this":
@@ -17,6 +23,48 @@ switch (userInput) {
         break;
 };
 
+// COMMAND FUNCTIONS
+
 function showTweets() {
+
+    var Twitter = require("twitter");
+    
+    var client = new Twitter({
+        consumer_key: keys.twitterKeys.consumer_key,
+        consumer_secret: keys.twitterKeys.consumer_secret,
+        access_token_key: keys.twitterKeys.access_token_key,
+        access_token_secret: keys.twitterKeys.access_token_secret
+    });
+    
+    var params = {
+        count: 20,
+    };
+
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (!error) {
+            console.log(tweets);
+        }
+    });
+};
+
+function spotifySong() {
+
+    var Spotify = require('node-spotify-api');
+
+    var spotify = new Spotify({
+        id: "342380a4e1574dcea4a9500716e28c1d",
+        secret: "fec13ce1905749b8a6df6f916864b9d0",
+    });
+
+    spotify.search({
+        type: 'track',
+        query: 'All the Small Things'
+    }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+
+        console.log(data);
+    });
 
 };
