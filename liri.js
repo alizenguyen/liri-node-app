dc08c61c87c768b82279055f260d8461e90b1542
Alize Nguyen <alizenguyen@gmail.com>
2017-11-19 14:34:51 -0600

    commands created

---
+++ liri.js
@@ -0,0 +1,22 @@
+var keys = require("./keys.js");
+
+var userInput = processargv[2];
+
+switch (userInput) {
+    case "my-tweets":
+        showTweets();
+        break;
+
+    case "spotify-this-song":
+        break;
+
+    case "movie-this":
+        break;
+
+    case "do-what-it-says":
+        break;
+};
+
+function showTweets() {
+
+};
