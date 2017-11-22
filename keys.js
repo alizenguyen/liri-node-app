f87726a9655f96f4ac7e2ca60fd1e1e191829ed7
Alize Nguyen <alizenguyen@gmail.com>
2017-11-21 18:31:22 -0600

    key error update

--- keys.js
+++ keys.js
@@ -1,10 +1,10 @@
 console.log('this is loaded');
 
 var twitterKeys = {
-  consumer_key: '<IzCIWEXrA8OQEjM7tYvAjhsb0>',
-  consumer_secret: '<aSmN4BYYY3Tmx9Yva7oNfxPvZaSqM7DPGbJQ0zDFcgfhrOFww1>',
-  access_token_key: '<932340460697407489-H1gkAWUXKOP2dlDRo9rLc18ELUxutvT>',
-  access_token_secret: '<SxWoYsGWLzFNwxHulTOj5XQduC512e3FozBjm82mukXQG>',
+  consumer_key: 'IzCIWEXrA8OQEjM7tYvAjhsb0',
+  consumer_secret: 'aSmN4BYYY3Tmx9Yva7oNfxPvZaSqM7DPGbJQ0zDFcgfhrOFww1',
+  access_token_key: '932340460697407489-H1gkAWUXKOP2dlDRo9rLc18ELUxutvT',
+  access_token_secret: 'SxWoYsGWLzFNwxHulTOj5XQduC512e3FozBjm82mukXQG',
 }
 
 module.exports = twitterKeys;
